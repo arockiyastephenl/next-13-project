1ced6d774db0bffbf48f2cc09805f38e96051f27
Arockiya stephen <lourdustephen95@gmail.com>
2023-09-18 15:50:21 +0530

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "Todo App",
+  title: "ERP App",
 }
 
 export default function RootLayout({
